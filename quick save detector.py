364940472ab250cf3379d18dca2e431f31846a50
Alan Baines <abaines@vt.edu>
2020-11-06 18:25:30 -0500

    Create quick save detector.py

---
+++ quick save detector.py
@@ -0,0 +1,19 @@
+# Alan Baines
+
+import glob
+import hashlib
+import io
+import os
+import random
+import re
+import shutil
+import string
+import sys
+import threading
+import time
+import winsound
+
+# TODO
+
+print("Startup sequence complete")
+
